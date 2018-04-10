b480a18a41577da3df6738d75babe8636f3193bf
pqmcgill <pqmcgill@gmail.com>
2018-04-10 19:29:00 -0400

    support selecting events from document

---
+++ src/DocumentDOMSource.ts
@@ -0,0 +1,23 @@
+import xs, { Stream, Producer } from 'xstream';
+import { ReactSource } from './ReactSource';
+
+export class DocumentDOMSource implements ReactSource {
+  public select(selector: string): Stream<Event> {
+    return this;
+  }
+
+  public event(eventType: string): Stream<Event> {
+    return xs.create({
+      next: null,
+      start(listener) {
+        this.next = function(event) {
+          listener.next(event);
+        }
+        document.addEventListener(eventType, this.next);
+      },
+      stop() {
+        document.removeEventListener(eventType, this.next);
+      }
+    } as Producer<Event>);
+  }
+}
--- src/reactSource.ts
+++ src/reactSource.ts
@@ -2,13 +2,14 @@ import xs, { Stream } from 'xstream';
 import { createElement, ReactElement } from 'react';
 import { getHandler } from './handlers';
 import Provider from './contextProvider';
+import { DocumentDOMSource } from './DocumentDOMSource';
 
 export interface ReactSource {
     select(selector: string): ReactSource;
     event(eventType: string): Stream<any>
 }
 
-export class MainReactSource {
+export class MainReactSource implements ReactSource {
   private scope?: string;
   private selector: string;
 
@@ -17,11 +18,20 @@ export class MainReactSource {
     this.selector = selector || '';
   }
 
-  select(selector: string): ReactSource {
-    return new MainReactSource(selector, this.scope);
+  private getNamespacedSelector(): string {
+    return this.scope ? `${this.selector}_${this.scope}` : this.selector;
   }
 
-  event(eventType: string): Stream<any> {
+  public select(selector: string): ReactSource;
+  public select(selector: string): DocumentDOMSource {
+    if (selector === 'document') {
+      return new DocumentDOMSource();
+    } else {
+      return new MainReactSource(selector, this.scope);
+    }
+  }
+
+  public event(eventType: string): Stream<any> {
     if (!this.selector.length) {
       return xs.empty();
     } else {
@@ -29,15 +39,11 @@ export class MainReactSource {
     }
   }
 
-  getNamespacedSelector(): string {
-    return this.scope ? `${this.selector}_${this.scope}` : this.selector;
-  }
-
-  isolateSource(source: ReactSource, namespace: string): ReactSource {
+  public isolateSource(source: ReactSource, namespace: string): ReactSource {
     return new MainReactSource(this.selector, namespace);
   }
 
-  isolateSink(sink: Stream<ReactElement<any>>, namespace: string): Stream<ReactElement<any>> {
+  public isolateSink(sink: Stream<ReactElement<any>>, namespace: string): Stream<ReactElement<any>> {
     return sink.map(vtree => {
       return createElement(Provider, { scope: namespace }, vtree);
     })
