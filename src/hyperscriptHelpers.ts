f17c19028f4b7e0ee35e21b47aaa37c871155fcd
pqmcgill <pqmcgill@gmail.com>
2018-04-13 17:06:16 -0400

    added hyperscript helpers

---
+++ src/hyperscriptHelpers.ts
@@ -0,0 +1,239 @@
+import { h } from './h';
+import * as React from 'react';
+
+function createTagFunction(tagName: string): Function {
+  return function hyperscript(props?: any, children?: any) {
+    const hasProps = typeof props !== 'undefined';
+    const hasChildren = typeof children !== 'undefined';
+    if (hasProps && hasChildren) {
+      return h(tagName, props, ...children);
+    } else if (hasProps) {
+      return h(tagName, props);
+    } else {
+      return h(tagName, {});
+    }
+  }
+}
+
+const TAG_NAMES = [
+  'a',
+  'abbr',
+  'address',
+  'area',
+  'article',
+  'aside',
+  'audio',
+  'b',
+  'base',
+  'bdi',
+  'bdo',
+  'blockquote',
+  'body',
+  'br',
+  'button',
+  'canvas',
+  'caption',
+  'cite',
+  'code',
+  'col',
+  'colgroup',
+  'dd',
+  'del',
+  'dfn',
+  'dir',
+  'div',
+  'dl',
+  'dt',
+  'em',
+  'embed',
+  'fieldset',
+  'figcaption',
+  'figure',
+  'footer',
+  'form',
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'head',
+  'header',
+  'hgroup',
+  'hr',
+  'html',
+  'i',
+  'iframe',
+  'img',
+  'input',
+  'ins',
+  'kbd',
+  'keygen',
+  'label',
+  'legend',
+  'li',
+  'link',
+  'main',
+  'map',
+  'mark',
+  'menu',
+  'meta',
+  'nav',
+  'noscript',
+  'object',
+  'ol',
+  'optgroup',
+  'option',
+  'p',
+  'param',
+  'pre',
+  'progress',
+  'q',
+  'rp',
+  'rt',
+  'ruby',
+  's',
+  'samp',
+  'script',
+  'section',
+  'select',
+  'small',
+  'source',
+  'span',
+  'strong',
+  'style',
+  'sub',
+  'sup',
+  'table',
+  'tbody',
+  'td',
+  'textarea',
+  'tfoot',
+  'th',
+  'thead',
+  'time',
+  'title',
+  'tr',
+  'u',
+  'ul',
+  'video',
+];
+
+const exported = {
+  createTagFunction,
+  TAG_NAMES
+};
+
+TAG_NAMES.forEach(tagName => {
+  exported[tagName] = createTagFunction(tagName);
+});
+
+export default (exported as any) as HyperScriptHelpers;
+
+export interface HyperScriptHelperFn {
+  (props?: any, children?: any): React.ReactElement<any>;
+}
+
+export interface HyperScriptHelpers {
+  a: HyperScriptHelperFn;
+  abbr: HyperScriptHelperFn;
+  address: HyperScriptHelperFn;
+  area: HyperScriptHelperFn;
+  article: HyperScriptHelperFn;
+  aside: HyperScriptHelperFn;
+  audio: HyperScriptHelperFn;
+  b: HyperScriptHelperFn;
+  base: HyperScriptHelperFn;
+  bdi: HyperScriptHelperFn;
+  bdo: HyperScriptHelperFn;
+  blockquote: HyperScriptHelperFn;
+  body: HyperScriptHelperFn;
+  br: HyperScriptHelperFn;
+  button: HyperScriptHelperFn;
+  canvas: HyperScriptHelperFn;
+  caption: HyperScriptHelperFn;
+  cite: HyperScriptHelperFn;
+  code: HyperScriptHelperFn;
+  col: HyperScriptHelperFn;
+  colgroup: HyperScriptHelperFn;
+  dd: HyperScriptHelperFn;
+  del: HyperScriptHelperFn;
+  dfn: HyperScriptHelperFn;
+  dir: HyperScriptHelperFn;
+  div: HyperScriptHelperFn;
+  dl: HyperScriptHelperFn;
+  dt: HyperScriptHelperFn;
+  em: HyperScriptHelperFn;
+  embed: HyperScriptHelperFn;
+  fieldset: HyperScriptHelperFn;
+  figcaption: HyperScriptHelperFn;
+  figure: HyperScriptHelperFn;
+  footer: HyperScriptHelperFn;
+  form: HyperScriptHelperFn;
+  h1: HyperScriptHelperFn;
+  h2: HyperScriptHelperFn;
+  h3: HyperScriptHelperFn;
+  h4: HyperScriptHelperFn;
+  h5: HyperScriptHelperFn;
+  h6: HyperScriptHelperFn;
+  head: HyperScriptHelperFn;
+  header: HyperScriptHelperFn;
+  hgroup: HyperScriptHelperFn;
+  hr: HyperScriptHelperFn;
+  html: HyperScriptHelperFn;
+  i: HyperScriptHelperFn;
+  iframe: HyperScriptHelperFn;
+  img: HyperScriptHelperFn;
+  input: HyperScriptHelperFn;
+  ins: HyperScriptHelperFn;
+  kbd: HyperScriptHelperFn;
+  keygen: HyperScriptHelperFn;
+  label: HyperScriptHelperFn;
+  legend: HyperScriptHelperFn;
+  li: HyperScriptHelperFn;
+  link: HyperScriptHelperFn;
+  main: HyperScriptHelperFn;
+  map: HyperScriptHelperFn;
+  mark: HyperScriptHelperFn;
+  menu: HyperScriptHelperFn;
+  meta: HyperScriptHelperFn;
+  nav: HyperScriptHelperFn;
+  noscript: HyperScriptHelperFn;
+  object: HyperScriptHelperFn;
+  ol: HyperScriptHelperFn;
+  optgroup: HyperScriptHelperFn;
+  option: HyperScriptHelperFn;
+  p: HyperScriptHelperFn;
+  param: HyperScriptHelperFn;
+  pre: HyperScriptHelperFn;
+  progress: HyperScriptHelperFn;
+  q: HyperScriptHelperFn;
+  rp: HyperScriptHelperFn;
+  rt: HyperScriptHelperFn;
+  ruby: HyperScriptHelperFn;
+  s: HyperScriptHelperFn;
+  samp: HyperScriptHelperFn;
+  script: HyperScriptHelperFn;
+  section: HyperScriptHelperFn;
+  select: HyperScriptHelperFn;
+  small: HyperScriptHelperFn;
+  source: HyperScriptHelperFn;
+  span: HyperScriptHelperFn;
+  strong: HyperScriptHelperFn;
+  style: HyperScriptHelperFn;
+  sub: HyperScriptHelperFn;
+  sup: HyperScriptHelperFn;
+  table: HyperScriptHelperFn;
+  tbody: HyperScriptHelperFn;
+  td: HyperScriptHelperFn;
+  textarea: HyperScriptHelperFn;
+  tfoot: HyperScriptHelperFn;
+  th: HyperScriptHelperFn;
+  thead: HyperScriptHelperFn;
+  time: HyperScriptHelperFn;
+  title: HyperScriptHelperFn;
+  tr: HyperScriptHelperFn;
+  u: HyperScriptHelperFn;
+  ul: HyperScriptHelperFn;
+  video: HyperScriptHelperFn;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { makeReactDriver } from './makeReactDriver';
 import { ReactSource } from './ReactSource';
-import { h } from './h';
+import { h, ReactElement } from './h';
 
 export {
     makeReactDriver,
     ReactSource,
+    ReactElement,
     h
 };
